fa611d7fa9f5379f315757ec1ac14d66c12764fe
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-01 11:49:55 -0400

    Complete game() function and modify playRound()
    Modify the playRound() function to return a value, '1' if you win,
    '0' if you tie, and '-1' if you lose, and use this as a counter to
    see if the player ends up winning against the computer. The game()
    function keeps track of the wins and losses and runs the playRound()
    function 5 times.

--- javascript.js
+++ javascript.js
@@ -21,30 +21,37 @@ console.log(getComputerChoice()); */
 function playRound(playerSelection, computerSelection){
     playerSelection = capitalize(playerSelection);
     if (playerSelection === computerSelection){
-        return `You tie! ${playerSelection} cannot beat ${computerSelection}`;
+        console.log(`You tie! ${playerSelection} cannot beat ${computerSelection}`);
+        return 0;
     }
     if (playerSelection === "Rock"){
         if (computerSelection === "Scissors"){
-            return "You win! Rock beats Scissors";
+            console.log("You win! Rock beats Scissors");
+            return 1;
         }
         else if (computerSelection === "Paper"){
-            return "You lose! Paper beats Rock";
+            console.log("You lose! Paper beats Rock");
+            return -1;
         }
     }
     if (playerSelection === "Paper"){
         if (computerSelection === "Rock"){
-            return "You win! Paper beats Rock";
+            console.log("You win! Paper beats Rock");
+            return 1;
         }
         else if (computerSelection === "Scissors"){
-            return "You lose! Scissors beats Paper";
+            console.log("You lose! Scissors beats Paper");
+            return -1;
         }
     }
     if (playerSelection === "Scissors"){
         if (computerSelection === "Paper"){
-            return "You win! Scissors beats Paper";
+            console.log("You win! Scissors beats Paper");
+            return 1;
         }
         else if (computerSelection === "Rock"){
-            return "You lose! Rock beats Scissors";
+            console.log("You lose! Rock beats Scissors");
+            return -1;
         }
     }
 }
@@ -59,3 +66,23 @@ const playerSelection = "rOck";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 */
+
+function game() {
+    let winCounter = 0
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Enter a move ('Rock', 'Paper', or 'Scissors'): ");
+        let computerSelection = getComputerChoice();
+        winCounter += playRound(playerSelection, computerSelection);
+    }
+    if (winCounter > 0){
+        return "You won against the computer!"
+    }
+    if (winCounter < 0){
+        return "You lost to the computer!"
+    }
+    else {
+        return "You ties with the computer!"
+    }
+}
+
+console.log(game());
