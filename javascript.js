4d26a22c83735cf811a953fdd3aac1c294fce79e
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-01 11:09:58 -0400

    Add capitalize function
    This function will be used to ensure that user inputs will be in
    the proper format (first letter is capitalized, rest are lowercase).

--- javascript.js
+++ javascript.js
@@ -11,6 +11,10 @@ function getComputerChoice() {
     }
 }
 
+function capitalize(string){
+    const str = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    return str;
+}
 /*
 console.log(getComputerChoice());
 console.log(getComputerChoice());
