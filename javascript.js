444052b9dfa559bc54d6124b75e52df133ceb99e
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-14 10:34:06 -0400

    Implement the ability to click a button to restart the game

--- javascript.js
+++ javascript.js
@@ -66,7 +66,7 @@ const playerSelection = "rOck";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 */
-
+/*
 function game() {
     let winCounter = 0 // Used to keep track of who's winning
     // for (let i = 0; i < 5; i++){
@@ -83,45 +83,104 @@ function game() {
 } // Adjusted the win conditions and removed the for loop
 
 // console.log(game());
+*/
 
 /* 
         Implementing the UI for the Rock Paper Scissors Game
 */
+// Function to refresh the browser
+function refresh() {
+    window.location.reload(true);
+}
 
-let totalScore = 0;
+// Variables for Human Player score and CPU Score
+let humanScore = 0;
+let cpuScore = 0;
+let score = 0
+
+// Creating text elements that are displayed on screen
 const container = document.querySelector('#main-div');
 const result = document.createElement('div');
 result.classList.add('result');
-const currentScore = document.createElement('p');
+result.style.display = "flex";
+result.style.justifyContent = "space-evenly";
+const finalResult = document.createElement('div');
+finalResult.classList.add('finalResult');
+const hScore = document.createElement('p');
+const cScore = document.createElement('p');
 
-currentScore.textContent = `Your current score is currently: ${totalScore}`;
+// Styling the hScore attribute
+hScore.textContent = `Your current score is: ${humanScore}`;
+hScore.style.width = "150px";
+hScore.style.height = "150px";
+hScore.style.backgroundColor = "rgb(155, 102, 102)";
+hScore.style.display = "flex";
+hScore.style.alignItems = "center";
+hScore.style.textAlign = "center";
+hScore.style.padding = "10px";
 
-result.appendChild(currentScore);
+// Styling the cScore attribute
+cScore.textContent = `The CPU's current score is: ${cpuScore}`;
+cScore.style.width = "150px";
+cScore.style.height = "150px";
+cScore.style.backgroundColor = "rgb(155, 102, 102)";
+cScore.style.display = "flex";
+cScore.style.alignItems = "center";
+cScore.style.textAlign = "center";
+cScore.style.padding = "10px";
+
+result.appendChild(hScore);
+result.appendChild(cScore);
 container.appendChild(result);
+container.appendChild(finalResult);
 
+// Creating a button to start a new game
+const newGame =  document.createElement('button');
+newGame.classList.add('newGame');
+newGame.addEventListener('click', refresh);
+newGame.textContent = "Click here to play again!"
 
+// const buttonDiv = document.querySelector('#button-div');
+// buttonDiv.style.display = "flex";
+// buttonDiv.style.alignItems = "center";
+// Iterating through each possible button choice and
+// simulating the playRound() function.
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-   button.style.width = "150px";
-   button.style.height = "150px";
-   button.addEventListener('click', (e) =>
+    button.style.width = "150px";
+    button.style.height = "150px";
+    button.addEventListener('click', function game()
    {
-    totalScore += playRound(button.className, getComputerChoice());
-    currentScore.textContent = `Your current score is currently: ${totalScore}`;
-    
-    if (totalScore === 5){
+    score = playRound(button.className, getComputerChoice());
+    // Adding points to each player depending on winner
+    if (score === 1){
+        humanScore += 1;
+    }
+    else if (score === -1){
+        cpuScore += 1;
+    }
+    else {
+        humanScore += 0;
+        cpuScore += 0;
+    }
+    hScore.textContent = `Your current score is: ${humanScore}`;
+    cScore.textContent = `The CPU's current score is: ${cpuScore} `;
+
+    // Checking the win condition for each player
+    if (humanScore === 5){
         const p2 = document.createElement('p');
         p2.textContent = "You have won against the CPU!"
-        result.appendChild(p2);
-        totalScore = 0;
+        finalResult.appendChild(p2);
+        finalResult.appendChild(newGame);
+        button.removeEventListener('click', game);
     } 
-    else if (totalScore === -5){
+    else if (cpuScore === 5){
         const p3 = document.createElement('p');
         p3.textContent = "You have lost against the CPU!"
-        result.appendChild(p3)
-        totalScore = 0;
+        finalResult.appendChild(p3)
+        finalResult.appendChild(newGame);
+        button.removeEventListener('click', game);
+        
     }
    })
 });
-
-// playRound(`${button.class}`, getComputerChoice())
